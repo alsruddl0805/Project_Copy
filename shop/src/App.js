e3757b90d6a5a366bddf507066aad8f73e8d6ed8
alsruddl0805 <68004793+alsruddl0805@users.noreply.github.com>
2022-07-17 18:26:16 +0900

    Route 경로 세팅

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,38 +2,43 @@ import './App.css';
 import { useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import bg from './img/001.jpg';
-import data from './data.js';
+import { listData, Detail } from './data.js';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
-  let [shoes, setShoes] = useState(data);
+  let [shoes, setShoes] = useState(listData);
 
    return (
     <div className="App"> 
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Ming's</Navbar.Brand>
+          <Navbar.Brand href="#home">
+          <Link to="/">Ming's</Link>
+          </Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">ABOUT</Nav.Link>
-            <Nav.Link href="#features">SHOP</Nav.Link>
-            <Nav.Link href="#pricing">MYPAGE</Nav.Link>
+            <Nav.Link href="#shop"><Link to="/shop">SHOP</Link></Nav.Link>
+            <Nav.Link href="#cart">CART</Nav.Link>
+            <Nav.Link href="#mypage">MYPAGE</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-
-      <div className="main-bg"></div>
-      <div className="main-bg-import" style={{'background' : 'no-repeat center/100% url(' + bg + ')', height: '300px'}}></div>
       
-      <div className="container">
+      <Routes>
+        <Route path="/" element={<div className="main-bg-import" style={{'background' : 'no-repeat center/100% url(' + bg + ')', height: '300px'}}></div>} />
+        <Route path="/shop" element={
+        <div className="container">
         <div className="row">
           {
             shoes.map((item, i) => {
               return(
-                <List item={shoes[i]} i={i+1}/>
+                <List item={shoes[i]} i={i+1} key={i}/>
               )
             })
           }
         </div>
-      </div>
+      </div>} />
+      <Route path="/detail" element={<Detail/>} />
+      </Routes>
     </div>
   );
 }
@@ -41,9 +46,11 @@ function App() {
 function List(props) {
   return(
     <div className="col-md-4">
+      <Link to="/detail" >
       <img src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"} width="80%" />
       <h4>{ props.item.title }</h4>
       <p>{ props.item.price }</p>
+      </Link>
     </div> 
   )
 }
--- shop/src/data.js
+++ shop/src/data.js
@@ -1,4 +1,4 @@
-let data = [
+let listData = [
     {
       id : 0,
       title : "White and Black",
@@ -21,4 +21,22 @@ let data = [
     }
   ]
 
-export default data;
+  function Detail() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+          </div>
+          <div className="col-md-6">
+            <h4 className="pt-5">상품명</h4>
+            <p>상품설명</p>
+            <p>120000원</p>
+            <button className="btn btn-danger">주문하기</button> 
+          </div>
+        </div>
+      </div> 
+    )
+  }
+
+export {listData, Detail};
