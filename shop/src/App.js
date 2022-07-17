9a5df5c1269228f9c78bb7f23b0573045eb2467b
alsruddl0805 <68004793+alsruddl0805@users.noreply.github.com>
2022-07-17 18:42:16 +0900

    useNavigate / Nested Route / Outlet

--- shop/src/App.js
+++ shop/src/App.js
@@ -2,23 +2,23 @@ import './App.css';
 import { useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import bg from './img/001.jpg';
-import { listData, Detail } from './data.js';
-import { Routes, Route, Link } from 'react-router-dom';
+import { listData, Detail } from './routes/data.js';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 
 function App() {
   let [shoes, setShoes] = useState(listData);
+  let navigate = useNavigate(); // 페이지 이동을 도와주는 함수
 
    return (
     <div className="App"> 
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">
-          <Link to="/">Ming's</Link>
-          </Navbar.Brand>
+          <Navbar.Brand onClick={() => {navigate('/')}}>Ming's</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#shop"><Link to="/shop">SHOP</Link></Nav.Link>
+            <Nav.Link onClick={() => {navigate('/shop')}}>SHOP</Nav.Link>
             <Nav.Link href="#cart">CART</Nav.Link>
             <Nav.Link href="#mypage">MYPAGE</Nav.Link>
+            <Nav.Link onClick={() => {navigate('/about')}}>ABOUT</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -38,9 +38,46 @@ function App() {
         </div>
       </div>} />
       <Route path="/detail" element={<Detail/>} />
+
+      {/* 
+      Nested Route (태그 내부 경로 연결)
+      <Route path="/about/member" element={<About/>} />
+      <Route path="/about/location" element={<About/>} /> 
+      */}
+      <Route path="/about" element={<About/>}>
+        <Route path="member" element={<div>멤버</div>} />
+        <Route path="location" element={<div>장소</div>} />
+      </Route>
+      
+      <Route path="/event" element={<Event/>}>
+        <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+        <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+      </Route>
+
+      {/* * (별표표시) : 오타 포함 위 경로 제외 모든 페이지 */}
+      <Route path="*" element={<div>404 Page</div>} />
       </Routes>
     </div>
   );
+}
+
+function Event() {
+  return (
+    <div>
+      <div>오늘의 이벤트</div>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div>
+    <div>회사 정보 페이지</div>
+    {/* 어떤 장소에 Nested route 내부 element를 보여줄지 ? */}
+    <Outlet></Outlet>
+    </div>
+  )
 }
 
 function List(props) {
