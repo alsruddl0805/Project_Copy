05f735f4afd357a28ed4fdfb078aab2ae1b609b0
MIN GYEONG KWON <alsruddl0805@naver.com>
2022-07-22 14:05:05 +0900

    state 보관 / 사용법

--- shop/src/routes/Cart.js
+++ shop/src/routes/Cart.js
@@ -1,6 +1,18 @@
+import { useReducer } from 'react';
 import { Table } from 'react-bootstrap'
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+let CustomButton = styled.button`
+    background-color: #000;
+    color: #fff;
+`
 
 function Cart() {
+    // store에 있던 모든 state를 가져와주는 함수
+    let cartItem = useSelector((state) => { return state.cartItem }); // or => state.user
+    console.log('안받아왔어?', cartItem);
+
     return (
         <div>
         <Table>
@@ -13,12 +25,18 @@ function Cart() {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                <td>1</td>
-                <td>안녕</td>
-                <td>안녕</td>
-                <td>안녕</td>
-                </tr>
+                {
+                    cartItem.map((i, idx) => {
+                        return (
+                            <tr key={i.id}>
+                            <td>{idx + 1}</td>
+                            <td>{i.name}</td>
+                            <td>{i.count}</td>
+                            <td><CustomButton>변경 버튼</CustomButton></td>
+                            </tr>
+                        )
+                    })
+                }
             </tbody>
         </Table> 
         </div>
--- shop/src/store.js
+++ shop/src/store.js
@@ -1,5 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+let user = createSlice({ // useState() 와 같은 역할
+    name: 'user', // state name
+    initialState: 'kim' // state 값
+})
+
+let stock = createSlice({
+    name: 'stock',
+    initialState: [10, 11, 12 ],
+})
+
+let cartItem = createSlice({
+    name: 'item',
+    initialState: [
+        {id : 0, name : 'White and Black', count : 2},
+        {id : 2, name : 'Grey Yordan', count : 1}
+      ]
+})
 
 export default configureStore({
-  reducer: { }
+  reducer: {
+    // 여기에 등록을 해야 사용 가능
+    user: user.reducer,
+    stock: stock.reducer,
+    cartItem: cartItem.reducer,
+   }
 }) 
